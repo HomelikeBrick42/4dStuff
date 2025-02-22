8a32c9840cc38165664b92812d10aa5ce049b14a
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2025-02-22 19:37:04 +1300

    Passing GpuCamera to the compute shader

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,25 @@
 #![deny(rust_2018_idioms)]
 
 use eframe::{egui, wgpu};
+use encase::{ShaderSize, ShaderType, UniformBuffer};
+
+#[derive(ShaderType)]
+struct GpuCamera {
+    pub position: cgmath::Vector4<f32>,
+    pub forward: cgmath::Vector4<f32>,
+    pub right: cgmath::Vector4<f32>,
+    pub up: cgmath::Vector4<f32>,
+    pub aspect: f32,
+}
 
 pub struct App {
-    ray_tracing_pipeline: wgpu::ComputePipeline,
     egui_texture: wgpu::Texture,
     egui_texture_bind_group_layout: wgpu::BindGroupLayout,
     egui_texture_bind_group: wgpu::BindGroup,
     egui_texture_id: egui::TextureId,
+    camera_uniform_buffer: wgpu::Buffer,
+    camera_bind_group: wgpu::BindGroup,
+    ray_tracing_pipeline: wgpu::ComputePipeline,
 }
 
 impl App {
@@ -41,10 +53,39 @@ impl App {
             wgpu::FilterMode::Nearest,
         );
 
+        let camera_bind_group_layout =
+            device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+                label: Some("Camera Bind Group Layout"),
+                entries: &[wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::COMPUTE,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: Some(GpuCamera::SHADER_SIZE),
+                    },
+                    count: None,
+                }],
+            });
+        let camera_uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("Camera Uniform Buffer"),
+            size: GpuCamera::SHADER_SIZE.get(),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            mapped_at_creation: false,
+        });
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("Camera Bind Group"),
+            layout: &camera_bind_group_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: camera_uniform_buffer.as_entire_binding(),
+            }],
+        });
+
         let ray_tracing_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("Ray Tracing Pipeline Layout"),
-                bind_group_layouts: &[&egui_texture_bind_group_layout],
+                bind_group_layouts: &[&egui_texture_bind_group_layout, &camera_bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -61,11 +102,13 @@ impl App {
             });
 
         Ok(Self {
-            ray_tracing_pipeline,
             egui_texture,
             egui_texture_bind_group_layout,
             egui_texture_bind_group,
             egui_texture_id,
+            camera_uniform_buffer,
+            camera_bind_group,
+            ray_tracing_pipeline,
         })
     }
 
@@ -100,6 +143,75 @@ impl App {
         });
         (egui_texture, egui_texture_view, egui_texture_bind_group)
     }
+
+    fn resize(&mut self, width: u32, height: u32, render_state: &eframe::egui_wgpu::RenderState) {
+        let eframe::egui_wgpu::RenderState {
+            device, renderer, ..
+        } = render_state;
+
+        let egui_texture_view;
+        (
+            self.egui_texture,
+            egui_texture_view,
+            self.egui_texture_bind_group,
+        ) = Self::egui_texture(
+            device,
+            width.max(1),
+            height.max(1),
+            &self.egui_texture_bind_group_layout,
+        );
+        renderer.write().update_egui_texture_from_wgpu_texture(
+            &render_state.device,
+            &egui_texture_view,
+            wgpu::FilterMode::Nearest,
+            self.egui_texture_id,
+        );
+    }
+
+    fn update_camera(&self, queue: &wgpu::Queue) {
+        let (render_width, render_height) = (self.egui_texture.width(), self.egui_texture.height());
+
+        let mut buffer = queue
+            .write_buffer_with(&self.camera_uniform_buffer, 0, GpuCamera::SHADER_SIZE)
+            .expect("the camera uniform buffer should be big enough to write a GpuCamera");
+        UniformBuffer::new(&mut *buffer)
+            .write(&GpuCamera {
+                aspect: render_width as f32 / render_height as f32,
+                position: cgmath::vec4(0.0, 0.0, 0.0, 0.0),
+                forward: cgmath::vec4(1.0, 0.0, 0.0, 0.0),
+                right: cgmath::vec4(0.0, 1.0, 0.0, 0.0),
+                up: cgmath::vec4(0.0, 0.0, 1.0, 0.0),
+            })
+            .expect("the buffer should be big enough to write a GpuCamera");
+    }
+
+    fn render(&self, render_state: &eframe::egui_wgpu::RenderState) {
+        let eframe::egui_wgpu::RenderState { device, queue, .. } = render_state;
+        let (render_width, render_height) = (self.egui_texture.width(), self.egui_texture.height());
+
+        let mut compute_encoder = device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+            label: Some("Compute Encoder"),
+        });
+        {
+            let mut ray_tracing_pass =
+                compute_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
+                    label: Some("Ray Tracing Pass"),
+                    timestamp_writes: None,
+                });
+
+            ray_tracing_pass.set_pipeline(&self.ray_tracing_pipeline);
+            ray_tracing_pass.set_bind_group(0, &self.egui_texture_bind_group, &[]);
+            ray_tracing_pass.set_bind_group(1, &self.camera_bind_group, &[]);
+
+            let workgroup_size = 16;
+            ray_tracing_pass.dispatch_workgroups(
+                render_width.div_ceil(workgroup_size),
+                render_height.div_ceil(workgroup_size),
+                1,
+            );
+        }
+        queue.submit(Some(compute_encoder.finish()));
+    }
 }
 
 impl eframe::App for App {
@@ -107,72 +219,19 @@ impl eframe::App for App {
         egui::CentralPanel::default()
             .frame(egui::Frame::NONE.fill(egui::Color32::from_rgb(255, 0, 255)))
             .show(ctx, |ui| {
-                ui.input(|i| {
-                    if i.pointer.button_down(egui::PointerButton::Secondary) {
-                        let _mouse_movement =
-                            i.pointer.motion().unwrap_or_else(|| i.pointer.delta());
-                        // TODO: mouse look
-                    }
-                });
-
-                let eframe::egui_wgpu::RenderState {
-                    device,
-                    queue,
-                    renderer,
-                    ..
-                } = frame
+                let render_state = frame
                     .wgpu_render_state()
                     .expect("the wgpu renderer should be in use");
 
                 let (_, rect) = ui.allocate_space(ui.available_size());
                 let (width, height) = (rect.width() as u32, rect.height() as u32);
 
-                if width > 0
-                    && height > 0
-                    && self.egui_texture.width() != width
-                    && self.egui_texture.height() != height
-                {
-                    let egui_texture_view;
-                    (
-                        self.egui_texture,
-                        egui_texture_view,
-                        self.egui_texture_bind_group,
-                    ) = Self::egui_texture(
-                        device,
-                        width,
-                        height,
-                        &self.egui_texture_bind_group_layout,
-                    );
-                    renderer.write().update_egui_texture_from_wgpu_texture(
-                        device,
-                        &egui_texture_view,
-                        wgpu::FilterMode::Nearest,
-                        self.egui_texture_id,
-                    );
+                if self.egui_texture.width() != width && self.egui_texture.height() != height {
+                    self.resize(width, height, render_state);
                 }
 
-                let mut compute_encoder =
-                    device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
-                        label: Some("Compute Encoder"),
-                    });
-                {
-                    let mut ray_tracing_pass =
-                        compute_encoder.begin_compute_pass(&wgpu::ComputePassDescriptor {
-                            label: Some("Ray Tracing Pass"),
-                            timestamp_writes: None,
-                        });
-
-                    ray_tracing_pass.set_pipeline(&self.ray_tracing_pipeline);
-                    ray_tracing_pass.set_bind_group(0, &self.egui_texture_bind_group, &[]);
-
-                    let workgroup_size = 16;
-                    ray_tracing_pass.dispatch_workgroups(
-                        width.div_ceil(workgroup_size),
-                        height.div_ceil(workgroup_size),
-                        1,
-                    );
-                }
-                queue.submit(std::iter::once(compute_encoder.finish()));
+                self.update_camera(&render_state.queue);
+                self.render(render_state);
 
                 ui.painter().image(
                     self.egui_texture_id,
--- src/ray_tracing.wgsl
+++ src/ray_tracing.wgsl
@@ -2,10 +2,27 @@
 @binding(0)
 var output_texture: texture_storage_2d<rgba8unorm, write>;
 
+struct Camera {
+    position: vec4<f32>,
+    forward: vec4<f32>,
+    right: vec4<f32>,
+    up: vec4<f32>,
+    aspect: f32,
+}
+
+@group(1)
+@binding(0)
+var<uniform> camera: Camera;
+
+struct Ray {
+    origin: vec4<f32>,
+    direction: vec4<f32>,
+}
+
 @compute
 @workgroup_size(16, 16, 1)
 fn main(
-    @builtin(global_invocation_id) global_id: vec3<u32>
+    @builtin(global_invocation_id) global_id: vec3<u32>,
 ) {
     let size = textureDimensions(output_texture);
     let coords = global_id.xy;
@@ -14,8 +31,12 @@ fn main(
         return;
     }
 
-    let uv = vec2<f32>(coords) / vec2<f32>(size);
+    var uv = ((vec2<f32>(coords) + 0.5) / vec2<f32>(size)) * 2.0 - 1.0;
 
-    let color = vec3<f32>(uv, 0.0);
+    var ray: Ray;
+    ray.origin = vec4<f32>(0.0, 0.0, 0.0, 0.0);
+    ray.direction = normalize(camera.right * (uv.x * camera.aspect) + camera.up * uv.y + camera.forward);
+
+    let color = ray.direction.xyz * 0.5 + 0.5;
     textureStore(output_texture, coords, vec4<f32>(clamp(color, vec3<f32>(0.0), vec3<f32>(1.0)), 1.0));
 }
