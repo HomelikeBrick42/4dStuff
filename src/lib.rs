b347256d40330cbdcf8f8a37cdde34e1588c0aa5
HomelikeBrick42 <64717463+HomelikeBrick42@users.noreply.github.com>
2025-02-25 17:45:39 +1300

    Moved the `Camera` logic to its own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,13 @@
 #![deny(elided_lifetimes_in_paths)]
 
 mod app;
+mod camera;
 pub mod gpu_buffer;
 pub mod rotor;
 
 pub use app::App;
 use eframe::egui;
-use rotor::Rotor;
 use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct Camera {
-    pub position: cgmath::Vector4<f32>,
-    pub base_rotation: Rotor,
-    pub vertical_angle: f32,
-
-    pub volume_view_enabled: bool,
-    /// a value between 0 and 1, 0 is no volume view, 1 is full volume view
-    pub volume_view_percentage: f32,
-}
-
-impl Camera {
-    pub const FORWARD: cgmath::Vector4<f32> = cgmath::vec4(1.0, 0.0, 0.0, 0.0);
-    pub const RIGHT: cgmath::Vector4<f32> = cgmath::vec4(0.0, 0.0, 1.0, 0.0);
-    pub const UP: cgmath::Vector4<f32> = cgmath::vec4(0.0, 1.0, 0.0, 0.0);
-    pub const ANA: cgmath::Vector4<f32> = cgmath::vec4(0.0, 0.0, 0.0, 1.0);
-
-    pub fn get_rotation(&self) -> Rotor {
-        self.base_rotation
-            * Rotor::rotation_xy(self.vertical_angle * (1.0 - self.volume_view_percentage))
-            * Rotor::rotation_yw(std::f32::consts::FRAC_PI_2 * self.volume_view_percentage)
-    }
-}
 
 #[derive(Serialize, Deserialize)]
 struct HyperSphere {
@@ -63,61 +39,6 @@ impl DrawUi for cgmath::Vector4<f32> {
                 .add(egui::DragValue::new(&mut self.w).prefix("w: ").speed(0.1))
                 .changed();
         });
-        changed
-    }
-}
-
-impl DrawUi for Camera {
-    fn draw_ui(&mut self, ui: &mut egui::Ui) -> bool {
-        let mut changed = false;
-
-        ui.horizontal(|ui| {
-            ui.label("Position: ");
-            changed |= self.position.draw_ui(ui);
-        });
-
-        ui.add_enabled_ui(false, |ui| {
-            let rotation = self.get_rotation();
-            let mut forward = rotation.rotate(Self::FORWARD);
-            let mut right = rotation.rotate(Self::RIGHT);
-            let mut up = rotation.rotate(Self::UP);
-            let mut ana = rotation.rotate(Self::ANA);
-
-            ui.horizontal(|ui| {
-                ui.label("Forward: ");
-                forward.draw_ui(ui);
-            });
-            ui.horizontal(|ui| {
-                ui.label("Right: ");
-                right.draw_ui(ui);
-            });
-            ui.horizontal(|ui| {
-                ui.label("Up: ");
-                up.draw_ui(ui);
-            });
-            ui.horizontal(|ui| {
-                ui.label("Ana: ");
-                ana.draw_ui(ui);
-            });
-        });
-
-        if ui.button("Reset Rotation").clicked() {
-            self.base_rotation = Rotor::IDENTITY;
-            self.vertical_angle = 0.0;
-            changed = true;
-        }
-
-        ui.horizontal(|ui| {
-            ui.label("Volume View: ");
-            ui.checkbox(&mut self.volume_view_enabled, "");
-        });
-
-        ui.horizontal(|ui| {
-            ui.label("Vertical Angle: ");
-            changed |= ui.drag_angle(&mut self.vertical_angle).changed();
-            ui.label("(not used in volume view)");
-        });
-
         changed
     }
 }
